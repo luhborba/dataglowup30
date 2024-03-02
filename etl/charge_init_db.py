96730c8096509e0615c48e61223c56ea2cf74ee2
Luciano Borba <luhborbafilho@gmail.com>
2024-03-02 10:01:47 -0300

    refactor: refatorando codigo charge init e main

--- etl/charge_init_db.py
+++ etl/charge_init_db.py
@@ -1,31 +1,28 @@
-import os
 import duckdb
-import glob
-import pandas as pd
-from typing import List
 
 from decorators.utils_decorators import log_decorator, time_measure_decorator
 
 @log_decorator
 @time_measure_decorator
-def load_csv_charge_duckdb(path: str) -> pd.DataFrame:
+def load_csv_charge_duckdb(path: str, table_name: str):
     """
     Esta função carrega os arquivos da pasta `path` e insere no banco de dados.
     
     args:
         path (str): Caminho da pasta de arquivos.
+        table_name (str): Nome da tabela no DuckDB.
     """
     # Conexão com o DuckDB
     con = duckdb.connect(database='duckdb.db', read_only=False)
     
     # Excluindo a tabela se existir
-    con.execute("DROP TABLE IF EXISTS ecomerce_events")
+    con.execute(f"DROP TABLE IF EXISTS {table_name}")
     
     # Select dos arquivos CSV
     query = f'''SELECT * FROM "{path}/*.csv"'''
     
     # Criando tabela no banco duckdb, criado na variavel 'con' de acordo com a variavel 'query'
-    con.execute(f' CREATE TABLE ecomerce_events AS {query}')
+    con.execute(f' CREATE TABLE {table_name} AS {query}')
 
     # Fechando a conexão com o DuckDB
     con.close()
@@ -33,7 +30,8 @@ def load_csv_charge_duckdb(path: str) -> pd.DataFrame:
     
 if __name__ == "__main__":
     path = "data/"
-    load_csv_charge_duckdb(path)
+    table_name = "ecomerce_events"
+    load_csv_charge_duckdb(path, table_name)
   
 
 
--- main.py
+++ main.py
@@ -4,8 +4,9 @@ from etl.charge_init_db import load_csv_charge_duckdb
 # Parametros
 dataset: str = "mkechinov/ecommerce-events-history-in-cosmetics-shop"
 path: str = "data/"
+table_name = "ecomerce_events"
 
 # Baixando Arquivos do Kaggle
 download_kaggle_dataset(dataset, path)
 # Carregando Arquivos no DuckDB
-load_csv_charge_duckdb(path)
+load_csv_charge_duckdb(path, table_name)
