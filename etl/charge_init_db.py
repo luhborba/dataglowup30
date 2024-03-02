1609e93716fafd4291777c918c0b58926c208ddc
Luciano Borba <luhborbafilho@gmail.com>
2024-03-02 00:11:23 -0300

    fix(etl): removendo o pandas do charge_init_db

--- etl/charge_init_db.py
+++ etl/charge_init_db.py
@@ -10,7 +10,7 @@ from decorators.utils_decorators import log_decorator, time_measure_decorator
 @time_measure_decorator
 def load_csv_charge_duckdb(path: str) -> pd.DataFrame:
     """
-    Esta função carrega os arquivos da pasta `path`, transforma-os em um único DataFrame e o retorna.
+    Esta função carrega os arquivos da pasta `path` e insere no banco de dados.
     
     args:
         path (str): Caminho da pasta de arquivos.
@@ -21,7 +21,7 @@ def load_csv_charge_duckdb(path: str) -> pd.DataFrame:
     # Excluindo a tabela se existir
     con.execute("DROP TABLE IF EXISTS ecomerce_events")
     
-    # Retornando o DataFrame do sql gerado no con.sql
+    # Select dos arquivos CSV
     query = f'''SELECT * FROM "{path}/*.csv"'''
     
     # Criando tabela no banco duckdb, criado na variavel 'con' de acordo com a variavel 'query'
@@ -30,27 +30,10 @@ def load_csv_charge_duckdb(path: str) -> pd.DataFrame:
     # Fechando a conexão com o DuckDB
     con.close()
     
-@log_decorator
-@time_measure_decorator
-def carregando_arquivos(path: str) -> List[pd.DataFrame]:
-    """
-    Esta função deve carregar os arquivos presentes na pasta data, transformar em uma lista de dataframes, concatena-los e retornar em um unico arquivo
-    """
-    arquivos = glob.glob(os.path.join(path, '*.csv'))
-    list_df = []
-    
-    for arquivo in arquivos:
-        list_df.append(pd.read_csv(arquivo))
-
-    # concatenando Lista de DataFrames
-    df = pd.concat(list_df)
-    
-    # salvando DataFrame em um arquivo
-    df.to_csv('data.csv', index=False)
     
 if __name__ == "__main__":
     path = "data/"
-    df = load_csv_charge_duckdb(path)
-    lista = carregando_arquivos(path)
+    load_csv_charge_duckdb(path)
+  
 
 
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from etl.extract import download_kaggle_dataset
-from etl.charge_init_db import carregando_arquivos, load_csv_charge_duckdb
+from etl.charge_init_db import load_csv_charge_duckdb
 
 # Parametros
 dataset: str = "mkechinov/ecommerce-events-history-in-cosmetics-shop"
@@ -9,5 +9,3 @@ path: str = "data/"
 download_kaggle_dataset(dataset, path)
 # Carregando Arquivos no DuckDB
 load_csv_charge_duckdb(path)
-# Carregando Arquivo em formato no pandas
-carregando_arquivos(path)
